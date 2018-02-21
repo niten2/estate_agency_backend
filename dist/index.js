84cbbdee066ca402c2967855aa002580d899a15f
Niten <niten2@yandex.ru>
2018-02-21 22:58:52 +0700

    remove dist

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const app_1 = require("./config/app");
-app_1.listen(express());
-//# sourceMappingURL=index.js.map
